d6577a555b831a9c566ddebcbb16b28933c129b4
austin <austin.hwang@kakaoenterprise.com>
2024-11-11 08:26:32 +0900

    :necktie: Feat: Query Parameter 사용해보기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
@@ -33,8 +34,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getMovies() {
-    return this.movies;
+  getMovies(@Query('title') title?: string) {
+    if (!title) {
+      return this.movies;
+    }
+
+    return this.movies.filter((m) => m.title.startsWith(title));
   }
 
   @Get(':id')
