2075f24d2ff302f347c45735b0a4f3563c92d27e
austin <austin.hwang@kakaoenterprise.com>
2024-11-12 08:55:43 +0900

    :necktie: Feat: PATCH DELETE 엔드포인트 전환하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('movie')
@@ -20,29 +29,13 @@ export class AppController {
     return this.appService.createMovie(title);
   }
 
-  // @Patch(':id')
-  // patchMovie(@Param('id') id: string, @Body('title') title: string) {
-  //   const movie = this.movies.find((m) => m.id === +id);
-  //
-  //   if (!movie) {
-  //     throw new NotFoundException('존재하지 않는 ID의 영화입니다!');
-  //   }
-  //
-  //   Object.assign(movie, { title });
-  //
-  //   return movie;
-  // }
-  //
-  // @Delete(':id')
-  // deleteMovie(@Param('id') id: string) {
-  //   const movieIndex = this.movies.findIndex((m) => m.id === +id);
-  //
-  //   if (movieIndex === -1) {
-  //     throw new NotFoundException('존재하지 않는 ID의 영화입니다!');
-  //   }
-  //
-  //   this.movies.splice(movieIndex, 1);
-  //
-  //   return id;
-  // }
+  @Patch(':id')
+  patchMovie(@Param('id') id: string, @Body('title') title: string) {
+    return this.appService.updateMovie(+id, title);
+  }
+
+  @Delete(':id')
+  deleteMovie(@Param('id') id: string) {
+    return this.appService.deleteMovie(+id);
+  }
 }
