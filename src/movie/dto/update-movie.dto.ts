bb98f8cb86e8d94b7818baef42cf3e82bfaf9535
austin <austin.hwang@kakaoenterprise.com>
2024-11-13 08:19:37 +0900

    :necktie: Feat: Custom Validator

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -27,11 +27,44 @@ import {
   MinLength,
   NotContains,
   NotEquals,
+  registerDecorator,
+  Validate,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
 } from 'class-validator';
 
 enum MovieGenre {
   Fantasy = 'fantasy',
   Action = 'action',
+}
+
+@ValidatorConstraint({
+  async: true,
+})
+class PasswordValidator implements ValidatorConstraintInterface {
+  validate(
+    value: any,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> | boolean {
+    /// 비밀번호 길이는 4-8
+    return value.length > 4 && value.length < 8;
+  }
+  defaultMessage?(validationArguments?: ValidationArguments): string {
+    return '비밀번호의 길이는 4~8자 여야합니다. 입력된 비밀번호: ($value)';
+  }
+}
+
+function IsPasswordValid(validateOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validateOptions,
+      validator: PasswordValidator,
+    });
+  };
 }
 
 export class UpdateMovieDto {
@@ -74,5 +107,11 @@ export class UpdateMovieDto {
   // @MinLength(4)
   // @IsUUID()
   // @IsLatLong()
+  // @Validate(PasswordValidator, {
+  //   message: '다른 에러 메시지',
+  // })
+  // @IsPasswordValid({
+  //   message: '다른 메시지',
+  // })
   test: string;
 }
