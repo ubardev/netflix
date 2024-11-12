d4cdbda8f4ae6bcb2c7e361e476e0e71b13dc3cc
austin <austin.hwang@kakaoenterprise.com>
2024-11-13 08:22:36 +0900

    :necktie: Feat: ValidationPipe 주요 옵션 알아보기

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Test API')
