590dbb9f726ae9c47adc2f371555a16d028e90e0
austin <austin.hwang@kakaoenterprise.com>
2024-11-13 08:08:47 +0900

    :necktie: Feat: 숫자/문자 Vadidator 정리

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,13 +1,38 @@
 import {
+  Contains,
   Equals,
+  IsAlphanumeric,
+  IsArray,
+  IsBoolean,
+  IsCreditCard,
+  IsDateString,
   IsDefined,
+  IsDivisibleBy,
   IsEmpty,
+  IsEnum,
+  IsHexColor,
   IsIn,
+  IsInt,
+  IsLatLong,
+  IsNegative,
   IsNotEmpty,
   IsNotIn,
   IsOptional,
+  IsPositive,
+  IsString,
+  IsUUID,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+  NotContains,
   NotEquals,
 } from 'class-validator';
+
+enum MovieGenre {
+  Fantasy = 'fantasy',
+  Action = 'action',
+}
 
 export class UpdateMovieDto {
   @IsNotEmpty()
@@ -28,5 +53,26 @@ export class UpdateMovieDto {
   /// Array
   // @IsIn(['action', 'fantasy'])
   // @IsNotIn(['action', 'fantasy'])
+  // @IsBoolean()
+  // @IsString()
+  // @IsString()
+  // @IsInt()
+  // @IsArray()
+  // @IsEnum(MovieGenre)
+  // @IsDateString()
+  // @IsDivisibleBy(5)
+  // @IsPositive()
+  // @IsNegative()
+  // @Max(100)
+  // @Min(50)
+  // @Contains('code factory')
+  // @NotContains('code factory')
+  // @IsAlphanumeric()
+  // @IsCreditCard()
+  // @IsHexColor()
+  // @MaxLength(16)
+  // @MinLength(4)
+  // @IsUUID()
+  // @IsLatLong()
   test: string;
 }
