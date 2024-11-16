878e6569e99d263694aba033cb076ff7dd840220
ubardev <bellin83@gmail.com>
2024-11-17 06:02:01 +0900

    :necktie: Feat: One to One Relationship 객체 만들어보기

--- src/movie/entity/movie.entity.ts
+++ src/movie/entity/movie.entity.ts
@@ -2,24 +2,21 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
   VersionColumn,
 } from 'typeorm';
+import { BaseTable } from './base-table.entity';
+import { MovieDetail } from './movie-detail.entity';
 
-export class BaseEntity {
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
-
-  @VersionColumn()
-  version: number;
-}
+/// ManyToOne Director -> 감독은 여러개의 영화를 마들 수 있음
+/// OneToOne MovieDetail -> 영화는 하나의 상세 내용을 가질 수 있음
+/// ManyToMany Genre -> 영화는 여러개의 장르를 가질 수 있고 장르는 여러개의 영화에 속할 수 있음
 
 @Entity()
-export class Movie extends BaseEntity {
+export class Movie extends BaseTable {
   @PrimaryGeneratedColumn()
   id: number;
 
@@ -28,4 +25,8 @@ export class Movie extends BaseEntity {
 
   @Column()
   genre: string;
+
+  @OneToOne(() => MovieDetail)
+  @JoinColumn()
+  detail: MovieDetail;
 }
