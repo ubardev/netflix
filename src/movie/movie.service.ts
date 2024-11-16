728ed89652644b7e7afc8e0548378cafbb52f943
ubardev <bellin83@gmail.com>
2024-11-17 06:25:28 +0900

    :necktie: Feat: One to One Relationship Create 작업 해보기

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -4,11 +4,14 @@ import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entity/movie.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Like, Repository } from 'typeorm';
+import { MovieDetail } from './entity/movie-detail.entity';
 
 @Injectable()
 export class MovieService {
   constructor(
     @InjectRepository(Movie) private movieRepository: Repository<Movie>,
+    @InjectRepository(MovieDetail)
+    private movieDetailRepository: Repository<MovieDetail>,
   ) {}
 
   getManyMovies(title?: string) {
@@ -35,7 +38,15 @@ export class MovieService {
   }
 
   async createMovie(createMovieDto: CreateMovieDto) {
-    const movie = await this.movieRepository.save(createMovieDto);
+    const movieDetail = await this.movieDetailRepository.save({
+      detail: createMovieDto.detail,
+    });
+
+    const movie = await this.movieRepository.save({
+      title: createMovieDto.title,
+      genre: createMovieDto.genre,
+      detail: movieDetail,
+    });
 
     return movie;
   }
