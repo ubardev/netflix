f9e9077aa8bfe1e5c646b94fd1df9003447a3b49
ubardev <bellin83@gmail.com>
2024-11-20 08:04:52 +0900

    :necktie: Feat: Custom Validation Pipe

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -14,6 +14,7 @@ import {
 import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { MovieTitleValidationPipe } from './pipe/movie-title-validation.pipe';
 
 @Controller('movie')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -21,7 +22,7 @@ export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
   @Get()
-  getMovies(@Query('title') title?: string) {
+  getMovies(@Query('title', MovieTitleValidationPipe) title?: string) {
     return this.movieService.findAll(title);
   }
 
