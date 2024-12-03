7db3ed9ab448314e21392f2e0884ffea5d57b671
ubardev <bellin83@gmail.com>
2024-12-04 08:08:10 +0900

    :necktie: Feat: Fobidden Exception Filter 구현하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,10 +21,11 @@ import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
 import { envVariableKeys } from './common/const/env.const';
 import { BearerTokenMiddleware } from './auth/middleware/bearer-token.middleware';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthGuard } from './auth/guard/auth.guard';
 import { RBACGuard } from './auth/guard/rbac.guard';
 import { ResponseTimeInterceptor } from './common/intercepter/response-time.interceptor';
+import { ForbiddenExceptionFilter } from './common/filter/fobidden.filter';
 
 @Module({
   imports: [
@@ -66,6 +67,7 @@ import { ResponseTimeInterceptor } from './common/intercepter/response-time.inte
     { provide: APP_GUARD, useClass: AuthGuard },
     { provide: APP_GUARD, useClass: RBACGuard },
     { provide: APP_INTERCEPTOR, useClass: ResponseTimeInterceptor },
+    { provide: APP_FILTER, useClass: ForbiddenExceptionFilter },
   ],
 })
 export class AppModule implements NestModule {
---
+++ src/common/filter/fobidden.filter.ts
@@ -0,0 +1,27 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  ForbiddenException,
+} from '@nestjs/common';
+import { timestamp } from 'rxjs';
+
+@Catch(ForbiddenException)
+export class ForbiddenExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status = exception.getStatus();
+
+    console.log(`[UnahthorizedException] ${request.method} f${request.path}`);
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: '권한이 없습니다!!!',
+    });
+  }
+}
