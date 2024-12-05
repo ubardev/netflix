ba6f5d569e50a903b8fce15609df1522498db138
ubardev <bellin83@gmail.com>
2024-12-06 07:53:12 +0900

    :necktie: Feat: MulterOptions 알아보기

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -14,6 +14,7 @@ import {
   UseGuards,
   UploadedFile,
   UploadedFiles,
+  BadRequestException,
 } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
@@ -54,16 +55,33 @@ export class MovieController {
   @RBAC(Role.admin)
   @UseInterceptors(TransactionInterceptor)
   @UseInterceptors(
-    FileFieldsInterceptor([
+    FileFieldsInterceptor(
+      [
+        {
+          name: 'movie',
+          maxCount: 1,
+        },
+        {
+          name: 'poster',
+          maxCount: 2,
+        },
+      ],
       {
-        name: 'movie',
-        maxCount: 1,
+        limits: {
+          fileSize: 20000000,
+        },
+        fileFilter(req, file, callback) {
+          if (file.mimetype !== 'video/mp4') {
+            return callback(
+              new BadRequestException('MP4 타입만 업로드 가능합니다!'),
+              false,
+            );
+          }
+
+          return callback(null, true);
+        },
       },
-      {
-        name: 'poster',
-        maxCount: 2,
-      },
-    ]),
+    ),
   )
   postMovie(
     @Body() body: CreateMovieDto,
