bad63dc3e334a64e871b9e6532ef8ec1d818fa2b
ubardev <bellin83@gmail.com>
2024-12-29 23:53:31 +0900

    :necktie: Feat: 다이나믹 타스크 스케쥴링

--- src/common/task.service.ts
+++ src/common/task.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, SchedulerRegistry } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { readdir, unlink } from 'fs/promises';
@@ -12,6 +12,7 @@ export class TaskService {
   constructor(
     @InjectRepository(Movie)
     private readonly movieRepository: Repository<Movie>,
+    private readonly schedulerRegistry: SchedulerRegistry,
   ) {}
 
   logEverySecond() {
@@ -50,7 +51,7 @@ export class TaskService {
     );
   }
 
-  @Cron('0 * * * * *')
+  //   @Cron('0 * * * * *')
   async calculateMovieLikeCounts() {
     console.log('run');
     await this.movieRepository.query(
@@ -74,5 +75,32 @@ export class TaskService {
           )
           `,
     );
+  }
+
+  @Cron('* * * * * *', {
+    name: 'printer',
+  })
+  pirnter() {
+    console.log('print every seconds');
+  }
+
+  @Cron('*/5 * * * * *')
+  stopper() {
+    console.log('---stopper run---');
+
+    const job = this.schedulerRegistry.getCronJob('printer');
+
+    // console.log('# Last Date');
+    // console.log(job.lastDate());
+    // console.log('# Next Date');
+    // console.log(job.nextDate());
+    console.log('# Next Dates');
+    console.log(job.nextDates(5));
+
+    if (job.running) {
+      job.stop();
+    } else {
+      job.start();
+    }
   }
 }
