3eb688eae1a1daf4fdc9895772096a238bbafe38
ubardev <bellin83@gmail.com>
2024-12-30 00:51:05 +0900

    :necktie: Feat: Custom Logger 제작하기

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -8,6 +8,7 @@ import { v4 } from 'uuid';
 import { TaskService } from './task.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Movie } from 'src/movie/entity/movie.entity';
+import { DefaultLogger } from './logger/default.logger';
 
 @Module({
   imports: [
@@ -33,7 +34,7 @@ import { Movie } from 'src/movie/entity/movie.entity';
     TypeOrmModule.forFeature([Movie]),
   ],
   controllers: [CommonController],
-  providers: [CommonService, TaskService],
-  exports: [CommonService],
+  providers: [CommonService, TaskService, DefaultLogger],
+  exports: [CommonService, DefaultLogger],
 })
 export class CommonModule {}
--- src/common/task.service.ts
+++ src/common/task.service.ts
@@ -7,15 +7,17 @@ import { join, parse } from 'path';
 import { Movie } from 'src/movie/entity/movie.entity';
 import { Repository } from 'typeorm';
 import { Logger } from '@nestjs/common';
+import { DefaultLogger } from './logger/default.logger';
 
 @Injectable()
 export class TaskService {
-  private readonly logger = new Logger(TaskService.name);
+  //   private readonly logger = new Logger(TaskService.name);
 
   constructor(
     @InjectRepository(Movie)
     private readonly movieRepository: Repository<Movie>,
     private readonly schedulerRegistry: SchedulerRegistry,
+    private readonly logger: DefaultLogger,
   ) {}
 
   @Cron('*/5 * * * * *')
