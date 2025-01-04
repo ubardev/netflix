67eda2d0187f7a9840739c4c6b71317f6cddee37
ubardev <bellin83@gmail.com>
2025-01-05 04:31:26 +0900

    :necktie: Feat: ApiProperty Annotation 알아보기

--- src/common/dto/cursor-pagination.dto.ts
+++ src/common/dto/cursor-pagination.dto.ts
@@ -1,19 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsArray, IsInt, IsOptional, IsString } from 'class-validator';
 
 export class CursorPaginationDto {
   @IsString()
   @IsOptional()
-  // id_52, likeCount_20
+  @ApiProperty({
+    description: '페이지네이션 커서',
+    example: 'eyJpZCI6MSwibG9jYXRpb24iOiJkZWZhdWx0In0',
+  })
   cursor?: string;
 
   @IsArray()
   @IsString({ each: true })
   @IsOptional()
+  @ApiProperty({
+    description: '내림차 또는 오름차 정렬',
+    example: ['id_DESC'],
+  })
+  @Transform(({ value }) => {
+    Array.isArray(value) ? value : [value];
+  })
   // id_ASC, id_DESC
   // [id_DESC, likeCount_DESC]
   order: string[] = ['id_DESC'];
 
   @IsInt()
   @IsOptional()
+  @ApiProperty({
+    description: '가져올 데이터 갯수',
+    example: 5,
+  })
   take: number = 5;
 }
