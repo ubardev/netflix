388a4b399f129ce259007e1db962bdd020502b1b
ubardev <bellin83@gmail.com>
2025-01-17 01:17:11 +0900

    :necktie: Feat: 에러 던지는 케이스 테스트하기

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { NotFoundException } from '@nestjs/common';
 
 const mockUserRepository = {
   findOne: jest.fn(),
@@ -47,5 +48,33 @@ describe('UserService', () => {
       expect(result).toEqual(users);
       expect(mockUserRepository.find).toHaveBeenCalled();
     });
+  });
+
+  describe('findOne', () => {
+    it('should return a user by id', async () => {
+      const user = { id: 1, email: 'test@codefactory.ai' };
+
+      jest.spyOn(mockUserRepository, 'findOne').mockResolvedValue(user);
+
+      const result = await userService.findOne(1);
+
+      expect(result).toEqual(user);
+      expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+        where: {
+          id: 1,
+        },
+      });
+    });
+
+    // it('should throw a NotFoundException if user is not found', async () => {
+    //   jest.spyOn(mockUserRepository, 'findOne').mockResolvedValue(null);
+
+    //   expect(userService.findOne(999)).rejects.toThrow(NotFoundException);
+    //   expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+    //     where: {
+    //       id: 999,
+    //     },
+    //   });
+    // });
   });
 });
